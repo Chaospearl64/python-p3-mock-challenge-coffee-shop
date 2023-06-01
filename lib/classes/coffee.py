f9060220e1cebc7c3538bcc4196b2d152f94f765
Cody Cogbill <codeman24@gmail.com>
2023-06-01 14:37:34 -0600

    done

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,26 @@
+
+
+
+
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
+
+        @property
+        def name(self):
+            return self._name
+
+    def orders(self):
         from classes.order import Order
-        pass
-    
-    def customers(self, new_customer=None):
-        from classes.customer import Customer
-        pass
-    
+        return [o for o in Order.all if o.coffee == self]
+
+    def customers(self):
+        return list(set([o.customer for o in self.orders()]))
+
     def num_orders(self):
-        pass
-    
+        return len(self.orders())
+
     def average_price(self):
-        pass
+      prices = [o.price for o in self.orders()]
+      total= sum(prices)
+      return total/len(prices)
--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,7 +1,11 @@
 
 class Order:
 
+    all =[]
+
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        self.all.append(self)
